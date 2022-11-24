ad43afb271be48389e4f39895510c9db22f0cd80
Jongbo Im <whdqh1550@gmail.com>
2022-11-23 23:56:23 -0600

    learn to make event query selector and submit
    submit is including hitting enter key
    queryselector needs to be write when it is in CSS

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const loginForm = document.getElementById("login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector(" button");
 
-function loginBtnListener() {
-  const value = loginInput.value;
-  if (value === "") {
-    alert("please fill the input");
-  } else if (value.length < 8) {
-    alert("make longer than 8 characters");
-  }
+function loginsSubmitListener(event) {
+  const userName = loginInput.value;
+  event.preventDefault();
+  console.log(userName);
 }
 
-loginButton.addEventListener("click", loginBtnListener);
-sdasdas;
+loginForm.addEventListener("submit", loginsSubmitListener);
